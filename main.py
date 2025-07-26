ed5fd81633c0c29f2d928d023ddddec102195864
btakahashi-90 <brandon.takahashi@outlook.com>
2025-07-26 10:09:44 -0700

    and now its a 'survival game', no shooting yet just see how long you last

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 import pygame
+import sys
 import constants as c
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
     pygame.init()
     # set screen
     screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+    # Groups setup
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     # FPS things
     clock = pygame.time.Clock()
     dt = 0
     # Player
     player = Player(c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2)
+    # Asteroid Field
+    field = AsteroidField()
     # game loop
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit(0)
+        for drawing in drawable:
+            drawing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
