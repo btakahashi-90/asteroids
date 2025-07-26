e06d2c6f99cc1caf194c1d21035990f27fa34381
btakahashi-90 <brandon.takahashi@outlook.com>
2025-07-26 07:25:50 -0700

    Initial commit. Constants started, main imports constants. Nothing fancy. :nod:

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import constants as c
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {c.SCREEN_WIDTH}")
+    print(f"Screen height: {c.SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
