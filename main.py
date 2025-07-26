f2e1d7315d45e2dec802104dcf58af84765a41f1
btakahashi-90 <brandon.takahashi@outlook.com>
2025-07-26 07:42:40 -0700

    dt calculated, fps capped at 60

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     pygame.init()
     # set screen
     screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+    # FPS things
+    clock = pygame.time.Clock()
+    dt = 0
     # game loop
     while(True):
         for event in pygame.event.get():
@@ -12,6 +15,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
